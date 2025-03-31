159c2aa46e06b0fcdb474fecb4c5fd3175231560
Federico Cola <colaf@MacBook-Air-di-Federico-2.local>
2025-03-31 11:52:47 +0200

    modifica di prova per testare CI

--- src/main/java/it/uniroma2/dicii/isw/Prova.java
+++ src/main/java/it/uniroma2/dicii/isw/Prova.java
@@ -2,6 +2,6 @@ package it.uniroma2.dicii.isw;
 
 public class Prova {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! i hate u");
     }
 }
